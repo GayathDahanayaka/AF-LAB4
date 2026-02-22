909e9bb6a68ebc25467291591d48953e55926177
GayathDahanayaka <gayathsanvidu@gmail.com>
2026-02-22 11:34:46 +0530

    Add Read File

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 console.log("This is app.js file");
 
+// Reading a file using fs module
 const fs = require('fs');
 fs.readFile('Read.txt', 'utf8', function (err, data) {
- if (err) throw err;
- console.log(data);
+    if (err) throw err;
+    console.log(data);
+});
+
+// Writing to a file using fs module
+fs.writeFile('Read.txt', 'Hello World!', (err) => {
+    if (err) throw err;
+    console.log('File saved');
 });
